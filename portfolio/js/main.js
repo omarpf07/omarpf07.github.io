926f5f4da04a9cfa751ca06743db39cc2c0de313
omarpf07 <heomsolutions@gmail.com>
2017-07-23 14:42:13 -0400

    Add files via upload

---
+++ portfolio/js/main.js
@@ -0,0 +1,10 @@
+function cambiar()
+{
+	var cita = "Especialista en tecnologías WEB.";
+	document.getElementById("quotes").innerHTML = cita;
+}
+document.getElementById("boton").onclick= function ()
+{
+	cambiar();
+}
+
