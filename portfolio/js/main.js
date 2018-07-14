74771a2ab419058045c5e2ed5099e58e74d3af34
Omar Perozo <omar.perozo07@gmail.com>
2018-07-14 13:27:51 -0400

    añadidas varias cosas para el evaluativo

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,10 +1,2 @@
-function cambiar()
-{
-	var cita = "Especialista en tecnologías WEB.";
-	document.getElementById("quotes").innerHTML = cita;
-}
-document.getElementById("boton").onclick= function ()
-{
-	cambiar();
-}
+
 
